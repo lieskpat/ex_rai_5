eb458885adbca21abb555bc31b77233f57d059df
lies <patrick.lieske@web.de>
2020-06-30 14:33:07 +0200

    initial commit

---
+++ test_r.R
@@ -0,0 +1,10 @@
+x1 <- sample(0:100, 1)
+x2 <- readline(prompt="Eine Zahl zwischen 0 - 100: ")
+as.integer(x2)
+if (x1 == x2) {
+	print("Die Zahl ist korrekt")
+} else {
+	cat("Die Zufallszahl ist: ", x1)
+	cat("\nIhre Zahl ist: ", x2)
+	print("\nVerloren")
+}
